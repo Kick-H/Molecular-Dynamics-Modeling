b571c3015cbfcd6dc09aa6a8f8e5c7b25f2d823e
Kick-H <30561696+Kick-H@users.noreply.github.com>
2021-01-11 10:36:02 +0800

    Update graphene.tcl
    Replace the create graphite file to graphene file

--- VMD/Graphene/graphene.tcl
+++ VMD/Graphene/graphene.tcl
@@ -1,90 +1,35 @@
+# length units: nm
+set lx 25
+set ly 25
+set bl 0.142
+set boxz 10.0
 
-proc BuildGraphene {lx ly bl boxz type nlayer isbond} {
+set type "armchair"
+set type "zigzag"
 
-    graphene -lx $lx -ly $ly -type $type -nlayers $nlayer -cc $bl -b $isbond -a $isbond -d $isbond -i $isbond -t C-C
+graphene -lx $lx -ly $ly -type $type -cc $bl -b 0 -a 0 -d 0 -i 0 -t C-C
 
-    set all [atomselect top all]
+set all [atomselect top all]
+set xmin [lindex [measure minmax $all] 0 0]
+set ymin [lindex [measure minmax $all] 0 1]
+$all moveby "[expr -$xmin] [expr -$ymin] 0.0"
 
-    set xmin [lindex [measure minmax $all] 0 0]
-    set ymin [lindex [measure minmax $all] 0 1]
-    $all moveby "[expr -$xmin] [expr -$ymin] 0.0"
+set xmin [lindex [measure minmax $all] 0 0]
+set ymin [lindex [measure minmax $all] 0 1]
+set xmax [lindex [measure minmax $all] 1 0]
+set ymax [lindex [measure minmax $all] 1 1]
+#set xcen [lindex [measure center $all] 0]
 
-    set xmin [lindex [measure minmax $all] 0 0]
-    set ymin [lindex [measure minmax $all] 0 1]
-    set xmax [lindex [measure minmax $all] 1 0]
-    set ymax [lindex [measure minmax $all] 1 1]
-
-    if { $nlayer == 1 } {
-
-        if { $type == "armchair" } {
-            set boxx [expr $xmax-$xmin+$bl*5*sqrt(3.0)]
-            set boxy [expr $ymax-$ymin+$bl*10]
-        } elseif { $type == "zigzag" } {
-            set boxx [expr $xmax-$xmin+$bl*10]
-            set boxy [expr $ymax-$ymin+$bl*5*sqrt(3.0)]
-        }
-
-    } else {
-
-        for { set a 0 }  {$a < $nlayer} {incr a} {
-
-            set nlaytop [expr $a * 3.35 + 0.1]
-            set nlaybot [expr $a * 3.35 - 0.1]
-            set newtype [expr $a + 1]
-            [atomselect top "z>$nlaybot and z<$nlaytop"] set name $newtype
-            [atomselect top "z>$nlaybot and z<$nlaytop"] set type $newtype
-            [atomselect top "z>$nlaybot and z<$nlaytop"] set element $newtype
-
-        }
-
-        if { $type == "armchair" } {
-            set boxx [expr $xmax-$xmin+$bl*5*sqrt(3.0)]
-            set boxy [expr $ymax-$ymin]
-        } elseif { $type == "zigzag" } {
-            set boxx [expr $xmax-$xmin+$bl*5]
-            set boxy [expr $ymax-$ymin]
-        }
-
-    }
-
-    pbc set "$boxx $boxy $boxz"
-
-    return [[atomselect top all] molid]
-
+if { $type == "armchair" } {
+    set boxx [expr $xmax-$xmin+$bl*5*sqrt(3.0)]
+    set boxy [expr $ymax-$ymin+$bl*10]
+} elseif { $type == "zigzag" } {
+    set boxx [expr $xmax-$xmin+$bl*10]
+    set boxy [expr $ymax-$ymin+$bl*5*sqrt(3.0)]
 }
 
+pbc set "$boxx $boxy $boxz"
 
-# lenth units: nm
-set lx 5
-set ly 5
-set bl 0.142
-set type "armchair"
-set nlayer 1
-set isbond 0
+topo writelammpsdata gra-$lx-$ly.data atomic
 
-# units: A, default layer distance: 3.35, which cannot be modified.
-set boxz [expr $nlayer * 3.35]
-
-# Single layer graphene. You should define the boxz firstly.
-# set boxz 10.0
-set multigra [BuildGraphene $lx $ly $bl $boxz $type $nlayer $isbond]
-
-topo writelammpsdata read.data full
-
-
-mol modmaterial 0 $multigra Diffuse
-mol material Diffuse
-mol modrep 0 $multigra
-mol modstyle 0 $multigra VDW 0.200000 50.000000
-mol addrep $multigra
-mol modstyle 1 $multigra DynamicBonds 1.600000 0.200000 12.000000
-
-display projection perspective
-display projection orthographic
-display depthcue off
-
-color Display Background white
-
-#topo guessangles
-#topo guessdihedrals
-#topo guessimpropers
+exit
